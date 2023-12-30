107c43c52a15764d9d82727b22a64972969af726
Juliano Almada <almadaj@edu.unifor.br>
2023-12-30 19:30:34 -0300

    criação do header de NoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
         <title>Home</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil"
+        />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
