a2c731e489d1fbb37582146d649fd28654aea0fc
Juliano Almada <almadaj@edu.unifor.br>
2024-01-04 17:13:44 -0300

    animações com o AOS

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ import CardsSection from "@/src/components/homeNoAuth/cardsSection";
 import SlideSection from "@/src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "@/src/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "@/src/components/common/footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -15,6 +17,9 @@ interface IndexPageProps {
 }
 
 const HomeNoAuth = ({ course }: IndexPageProps) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <Head>
@@ -27,12 +32,20 @@ const HomeNoAuth = ({ course }: IndexPageProps) => {
         />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div
+          className={styles.sectionBackground}
+          data-aos="fade-zoom-in"
+          data-aos-duration="1600"
+        >
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardsSection />
-        <SlideSection newestCourses={course} />
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={course} />
+        </div>
         <Footer />
       </main>
     </>
