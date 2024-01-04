b650c67bd7c90386d37fab4c729324fdae6cefb8
Juliano Almada <almadaj@edu.unifor.br>
2024-01-04 17:00:06 -0300

    footer implementado e HomeNoAuth completo

---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,25 @@
+import { Container } from "reactstrap";
+import styles from "./styles.module.scss";
+
+const Footer = function () {
+  return (
+    <>
+      <Container className={styles.footer}>
+        <img
+          src="/logoOnebitcode.svg"
+          alt="logoFooter"
+          className={styles.footerLogo}
+        />
+        <a
+          href="http://onebitcode.com"
+          target="_blank"
+          className={styles.footerLink}
+        >
+          onebitcode.com
+        </a>
+      </Container>
+    </>
+  );
+};
+
+export default Footer;
