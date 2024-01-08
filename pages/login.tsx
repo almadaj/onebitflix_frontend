857dc412790a731707b80ebf8c71f70055dadfd5
Juliano Almada <almadaj@edu.unifor.br>
2024-01-08 17:47:39 -0300

    primeira implementação de home autenticado

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,6 +15,12 @@ const Login = function () {
   const [toastMessage, setToastMessage] = useState("");
 
   useEffect(() => {
+    if (sessionStorage.getItem("onebitflix-token")) {
+      router.push("/home");
+    }
+  }, []);
+
+  useEffect(() => {
     const registerSuccess = router.query.registred;
     if (registerSuccess === "true") {
       setToastColor("bg-success");
