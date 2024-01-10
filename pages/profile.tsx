4787e44bbf2596a5904b1014883f83f19f7ac010
Juliano Almada <almadaj@edu.unifor.br>
2024-01-10 14:31:30 -0300

    seção de perfil implementada

---
+++ pages/profile.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import styles from "../styles/profile.module.scss";
+import UserForm from "@/src/components/profile/user";
+import HeaderAuth from "@/src/components/common/headerAuth";
+import { Button, Col, Container, Row } from "reactstrap";
+import Footer from "@/src/components/common/footer";
+
+const UserInfo = function () {
+  return (
+    <>
+      <Head>
+        <title>Onebitflix - Meu Perfil</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      </Head>
+      <main>
+        <HeaderAuth />
+        <Container className="py-5">
+          <p className={styles.title}>Minha Conta</p>
+          <Row className="pt-3 pb-5">
+            <Col md={4} className={styles.btnColumn}>
+              <Button className={styles.renderForm}>Dados Pessoais</Button>
+              <Button className={styles.renderForm}>Senha</Button>
+            </Col>
+            <Col md>
+              <UserForm />
+            </Col>
+          </Row>
+        </Container>
+        <div className={styles.footer}>
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default UserInfo;
